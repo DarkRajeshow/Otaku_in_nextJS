3200985efcc6581bab12dccfd1e408b10f578015
Rajesh Adeli <rajannaadeli@gmail.com>
2023-08-18 18:35:06 +0530

    Resolving some small npm run build errors

--- components/LinkToPages.js
+++ components/LinkToPages.js
@@ -24,7 +24,7 @@ export default function LinkToPages() {
                 <div className="grid1 px-10 py-8 rounded-[20px] text-left bg-black border-2 border-light relative">
                     <div className="col-span-4 p-5 bg-light top-0 left-0 absolute w-[103%] sm:w-[102%] md:w-[101.3%] lg:w-[103%] h-[103%] border-2 border-dark rounded-[30px] -z-10" />
                     <h1 className='text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-5 text-primaryDark'><FaArrowAltCircleRight className='mb-3 text-green' />Easily Serach, browse and discover anime in various categories.</h1>
-                    <p className='text-[12px] sm:text-sm md:text-base lg:text-md mb-5'>Excited to explore the awesome universe of anime series? No worries - our helpful search tool makes it a breeze to discover what you want! You can easily search by category, title, or release year. Let's embark on your anime journey together!</p>
+                    <p className='text-[12px] sm:text-sm md:text-base lg:text-md mb-5'>Excited to explore the awesome universe of anime series? No worries - our helpful search tool makes it a breeze to discover what you want! You can easily search by category, title, or release year. Let&apos;s embark on your anime journey together!</p>
                     <Link href={"/search"} >
                         <button className='flex items-center bg-light text-dark font-bold p-3 rounded-lg text-sm sm:text-base md:text-md lg:text-lg my-5 border-2 border-light hover:text-light hover:bg-dark transition-[300ms]'>
                             <FaSearch className='mr-2 text-primary' />Search Anime</button></Link>
